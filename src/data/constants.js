cf74403573e0c5e0309fc31b290d377610118cf6
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-08-06 12:41:57 +0530

    Chor: No commit message

---
+++ src/data/constants.js
@@ -0,0 +1,244 @@
+export const Bio = {
+  name: "Balaji",
+  roles: [
+    "Full Stack Web Developer",
+    "Programmer",
+    "Technical Trainer"
+  ],
+  description:
+    "I am a skilled full stack web developer, passionate about building dynamic and responsive web applications. With a strong foundation in both front-end and back-end technologies, I am dedicated to delivering high-quality solutions. My positive attitude and growth mindset drive me to continuously learn and take on new challenges, making meaningful contributions and achieving great results.",
+  github: "https://github.com/BALAJI-PRO-001",
+  resume:
+    "/Balaji_Reusme_Latest.pdf",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://img.icons8.com/color/48/000000/bootstrap.png",
+      },
+      {
+        name: "Sass",
+        image:
+          "https://img.icons8.com/color/48/000000/sass.png",
+      },
+      {
+        name: "Angular Js",
+        image:
+          "https://img.icons8.com/color/48/angularjs.png",
+      },
+      {
+        name: "Jquery",
+        image:
+          "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://img.icons8.com/fluency/48/000000/node-js.png",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://img.icons8.com/nolan/64/express-js.png"
+      },
+      {
+        name: "Type Script",
+        image:
+          "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-typescript-an-open-source-programming-language-developed-and-maintained-by-microsoft-logo-color-tal-revivo.png"
+      },
+      {
+        name: "C",
+        image:
+          "https://img.icons8.com/color/48/000000/c-programming.png"
+      },
+      {
+        name: "C++",
+        image:
+          "https://img.icons8.com/color/48/000000/c-plus-plus-logo.png"
+      },
+      {
+        name: "Java",
+        image:
+          "https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://img.icons8.com/color/48/000000/mongodb.png",
+      },
+      {
+        name: "DS&A",
+        image:
+          "https://img.icons8.com/external-flat-andi-nur-abdillah/64/external-Data-Structure-artificial-intelligence-(flat)-flat-andi-nur-abdillah.png",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://img.icons8.com/color/50/000000/git.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "IntelliJ",
+        image:
+          "https://img.icons8.com/color/48/intellij-idea.png",
+      },      
+      {
+        name: "Pycharm",
+        image:
+          "https://img.icons8.com/color/48/000000/pycharm.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-postman-is-the-only-complete-api-development-environment-logo-color-tal-revivo.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    img: "https://caddcae.com/wp-content/uploads/2019/01/Cadd-Logo-WEB.png",
+    role: "Technical Trainer and Final Year Project Developer",
+    company: "Cadd Cae Computers",
+    date: "Apr 2022 – Present",
+    desc: "As a Technical Trainer, I create and deliver training programs to help students and professionals improve their technical skills. I design course materials, teach classes, and keep up with the latest industry trends. As a Final Year Project Developer, I led a major project in my final year of college. I was responsible for designing and building the project, working with a team, and managing the development process to achieve a successful outcome.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Js",
+      "Ajax",
+      "Angular Js",
+      "Jquery",
+      "React",
+      "Node",
+      "Express Js",
+      "C",
+      "C++",
+      "Java",
+      "Python",
+      "MySql",
+      "Mongodb",
+      "Type Script"
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+];
+
+export const education = [
+  {
+    id: 1,
+    img: "https://images.shiksha.com/mediadata/images/1594975982phpeYyaPB.jpg",
+    school: "G.T.N College Of Arts, Dindigul",
+    date: "2020 - 2022",
+    grade: "80%",
+    desc: "A Bachelor of Science in Zoology is an undergraduate degree that focuses on the study of animals and their biology. The program typically covers a wide range of topics, including animal physiology, behavior, evolution, ecology, and conservation. Students gain a deep understanding of animal anatomy, genetics, and interactions with their environment. The degree equips graduates with the knowledge and skills necessary for careers in research, wildlife conservation, animal care, and environmental education, among other fields.",
+    degree: "Bsc",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWXyyLsjaBHL3MqtyjYfb-vZ1RvIfY-y7TOQ&s",
+    school: "Govt Hr Sec School Narasingapuram",
+    date: "2020 - 2022",
+    grade: "62.7%",
+    desc: "The Higher Secondary Certificate (HSC) is an educational qualification obtained after completing high school. It covers various subjects and prepares students for further education or vocational training.",
+    degree: "HSC",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWXyyLsjaBHL3MqtyjYfb-vZ1RvIfY-y7TOQ&s",
+    school: "Govt Hr Sec School Narasingapuram",
+    date: "2020 - 2022",
+    grade: "91.8%",
+    desc: "The Secondary School Leaving Certificate (SSLC) is awarded upon finishing secondary school. It marks the end of basic education and is required for further studies or entry into the workforce.",
+    degree: "SSLC",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Mern-Estate",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "RealEstateHub is a web application designed to simplify property management and enhance the real estate experience for both agents and users. It offers a user-friendly interface for agents to manage their property listings efficiently and for users to find their ideal properties with ease.",
+    image:
+      "https://raw.githubusercontent.com/BALAJI-PRO-001/Storage/main/Screenshot%20from%202024-08-06%2009-37-10.png",
+    tags: [
+      "React",
+      "Redux",
+      "Js",
+      "Node",
+      "Express js",
+      "MongoDb",
+    ],
+    category: "web app",
+    github: "https://github.com/BALAJI-PRO-001/MEAN_UMS",
+    webapp: "https://mern-estate-zlv0.onrender.com/",
+  }
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
